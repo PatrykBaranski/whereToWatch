b0dbd9f5903d2c588e9edba218eab8f8c96c546d
PatrykBaranski <pbaranski740@gmail.com>
2022-07-30 14:28:39 +0200

    Removing config file

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const API_GET_OPTIONS = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "fa8924fcc2msh521e55195e5dc8ap13c58djsn7c69d37d11bf",
-    "X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com",
-  },
-};
-export const API_URL =
-  "https://unogs-unogs-v1.p.rapidapi.com/title/details?netflix_id=";
